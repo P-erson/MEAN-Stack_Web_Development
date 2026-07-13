f68cc73318be93356da0b87331431c53bc22f99a
P-erson <mahmouds.fayed@outlook.com>
2026-07-13 18:44:26 +0300

    Add index.html and main.js files with initial structure and time greeting logic

---
+++ main.js
@@ -0,0 +1,9 @@
+var input = prompt("What time of day is it").toLowerCase();
+
+(input == 'morning') ? console.log('Good Morning') :
+(input == 'afternoon') ? console.log('Good Afternoon') :
+(input == 'night') ? console.log('Good Night') :
+console.log("Invalid time of day");
+
+
+
