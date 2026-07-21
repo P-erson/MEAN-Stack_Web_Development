bdda8b1c4965010ed312f5192f0aa24cd3e4bd28
P-erson <mahmouds.fayed@outlook.com>
2026-07-21 07:29:54 +0300

    Enhance form functionality in index.html and main.js, add update and delete features, and improve styles in styles.css

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+var database = [
+    {
+        'firstName': 'Mahmoud',
+        'lastName': 'Fayed',
+        'age': 19,
+        'isGraduated': false
+    }
+]
+
 var firstName = document.getElementById('firstName')
 var lastName = document.getElementById('lastName')
 var age = document.getElementById('age')
 var isGraduated = document.getElementById('isGraduated')
 
+var dataTable = document.getElementById('dataTable');
+
+console.log(database);
 
 function submit(){
     event.preventDefault();
@@ -12,10 +24,46 @@ function submit(){
         age: Number(age.value),
         isGraduated: isGraduated.checked
     };
+    database[database.length] = data;
+    console.log(database);
+    
+    // console.log(database.length);
+    dataTable.innerHTML += `<tr id="user${database.length - 1}"> <td class="tableCol">${data.firstName}</td> <td class="tableCol">${data.lastName}</td> <td class="tableCol">${data.age}</td> <td class="tableCol">${data.isGraduated}</td> <td><button onclick="retrieveData(${database.length - 1})">Update</button><button onclick="deleteUser(${database.length - 1})">Delete</button></td> </tr>`
+    
 
     firstName.value = '';
     lastName.value = '';
     age.value ='';
     isGraduated.checked = false;
+}
 
+function deleteUser(index) {
+    delete database[index];
+    console.log(database);
+    
+    document.getElementById(`user${index}`).innerHTML = null;
+}
+
+function retrieveData(index) {
+    firstName.value = database[index].firstName;
+    lastName.value = database[index].lastName;
+    age.value = database[index].age;
+    isGraduated.checked = database[index].isGraduated;
+
+    document.getElementById('updateBtn').outerHTML = `<button id="updateBtn" onclick="updateUser(${index})">Update</button>`
+}
+
+function updateUser(index){   
+    database[index].firstName = firstName.value;
+    database[index].lastName = lastName.value;
+    database[index].age = age.value;
+    database[index].isGraduated = isGraduated.checked;
+    
+
+    document.getElementById(`user${index}`).innerHTML = `<td class="tableCol">${firstName.value}</td> <td class="tableCol">${lastName.value}</td> <td class="tableCol">${age.value}</td> <td class="tableCol">${isGraduated.checked}</td> <td><button onclick="retrieveData(${database.length - 1})">Update</button><button onclick="deleteUser(${database.length - 1})">Delete</button></td>`
+
+    firstName.value = '';
+    lastName.value = '';
+    age.value ='';
+    isGraduated.checked = false;
 }
